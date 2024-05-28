645e6939567c0d211fcc92477048cc5f91d43f56
nakato156 <christianvelasces@gmail.com>
2024-05-28 00:52:03 -0500

    fix: hash password an update cookies

---
+++ app/controllers/cookie_ctrl.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken")
+
+const cookieCtrl = {}
+
+cookieCtrl.setCookie = (res, rememberMe, userForToken) => {
+    const JWT_EXPIRATION_LONG = process.env.JWT_EXPIRATION_LONG
+    const JWT_EXPIRATION_SHORT = process.env.JWT_EXPIRATION_SHORT
+
+    const timeExpiresIn = Number(rememberMe ? JWT_EXPIRATION_LONG : JWT_EXPIRATION_SHORT);
+    const expiresIn = rememberMe ? timeExpiresIn * 24 * 60 * 60 * 1000 : timeExpiresIn * 60 * 60 * 1000
+    const token = jwt.sign(userForToken, process.env.SECRET, {expiresIn: expiresIn});
+
+    res.cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: expiresIn // 7 días o 1 hora
+    });
+}
+
+module.exports = cookieCtrl;
